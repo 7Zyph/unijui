d33cca9327a45dfbf77cb5149c698e83189288c9
7Zyph <jp.buzanelo@gmail.com>
2023-10-31 23:15:31 -0300

    b

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,6 @@ import {
   IonContent
 } from '@ionic/react';
 
-
-
-
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
 
@@ -30,6 +27,19 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+let output = document.getElementById('output');
+function fetchData() {
+   fetch('https://api.kanye.rest')
+   .then(response => response.json())
+   .then(data => {
+      output.innerHTML += "Kanye :" + data.quote + "<br/>"; 
+   })
+}
+
+
+
+
+
 setupIonicReact();
 
 const App: React.FC = () => (
@@ -37,23 +47,33 @@ const App: React.FC = () => (
     <IonContent>
     <link rel="stylesheet" type="text/css" href="style.css" />
     <body>
+   
     <div class="myDiv">
         <h1>
     Bem-vindo ao meu site
         </h1>
         <p>Este é um parágrafo de exemplo.
         </p>
+       
         <IonButton id="present-alert">Clique Aqui</IonButton>
       <IonAlert
         trigger="present-alert"
-        message="Olá, Bem-vindo ao site interativo"
-        buttons={['OK']}
+        message= "Call Kanye?"
+        buttons={[
+                {
+                  text: "yes",
+                  handler: () =>{fetchData();}
+                }
+        ]}
       ></IonAlert>
-    </div>    
+
+    </div> 
+    <div id = "output"> </div>    
     <div class="myDiv">
         <h2>Seção importante</h2>
         <p>Esta é uma seção com conteúdo relevante.</p>
     </div>
+    
     <img src="logo.png"/>
     </body>
     </IonContent>
